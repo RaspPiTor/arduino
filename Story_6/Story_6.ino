6261e6f2e73bf15b21214bc1810c4ab2889b32bf
WeDon'tHaveAName <us@WeDon'tHaveAName.co.uk>
2017-04-04 18:46:23 +0000

    Created Story 5

---
+++ Story_6/Story_6.ino
@@ -0,0 +1,68 @@
+int const m1enable = 9;
+int const m1forward = 8;
+int const m1backward = 7;
+int const m2enable = 10;
+int const m2forward = 12;
+int const m2backward = 11;
+int const m1pin = 2;
+int const m2pin = 3;
+volatile int m1counter = 0;
+volatile int m2counter = 0;
+const int power = 150;
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(m1pin, INPUT);
+  pinMode(m2pin, INPUT);
+  attachInterrupt(0, m1change, RISING);
+  attachInterrupt(1, m2change, RISING);
+  pinMode(m1enable, OUTPUT);
+  pinMode(m1forward, OUTPUT);
+  pinMode(m1backward, OUTPUT);
+  pinMode(m2enable, OUTPUT);
+  pinMode(m2forward, OUTPUT);
+  pinMode(m2backward, OUTPUT);
+  digitalWrite(m1enable, HIGH);
+  digitalWrite(m2enable, HIGH);
+}
+
+void m1change() {
+  m1counter++;
+}
+
+void m2change() {
+  m2counter++;
+}
+
+void balance() {
+  if (m1counter > m2counter) {
+    analogWrite(m2forward, power);
+    analogWrite(m1forward, 0);
+  }
+  else if (m2counter > m1counter) {
+    analogWrite(m2forward, 0);
+    analogWrite(m1forward, power);
+  }
+  else {
+    analogWrite(m2forward, power);
+    analogWrite(m1forward, power);
+  }
+}
+
+void loop()
+{
+  balance();
+  if (m1counter > 1055 or m2counter > 1055) {
+    hardstop();
+    while (true) {
+    }
+  }
+}
+void hardstop() {
+  digitalWrite(m1enable, HIGH);
+  digitalWrite(m2enable, HIGH);
+  analogWrite(m1forward, 0);
+  analogWrite(m2forward, 0);
+}
+
