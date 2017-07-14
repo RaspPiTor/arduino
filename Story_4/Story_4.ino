52741417839b3063ab31d78f561bf1bb513ec60a
Hugh Williams <hughaw64@gmail.com>
2017-07-14 17:19:06 +0100

    Made Story 4 use functions instead of repeatadly assigning to pins and made it use a hard stop, does compile

--- Story_4/Story_4.ino
+++ Story_4/Story_4.ino
@@ -3,7 +3,8 @@ unsigned int const leftForward = 8;
 unsigned int const leftBackward = 7;
 unsigned int const rightEnable = 10;
 unsigned int const rightForward = 12;
-unsigned int const leftBackward = 11;
+unsigned int const rightBackward = 11;
+unsigned int const power = 171;
 
 void setup()
 {
@@ -12,26 +13,41 @@ void setup()
   pinMode(leftBackward, OUTPUT);
   pinMode(rightEnable, OUTPUT);
   pinMode(rightForward, OUTPUT);
-  pinMode(leftBackward, OUTPUT);
+  pinMode(rightBackward, OUTPUT);
+  digitalWrite(leftEnable, HIGH);
+  digitalWrite(rightEnable, HIGH);
 }
 
 void loop()
 {
+  forward();
+  delay(1000);
+  hardStop();
+  delay(1000);
+  backward();
+  delay(1000);
+  hardStop();
+}
+void hardStop()
+{
   digitalWrite(leftEnable, HIGH);
-  digitalWrite(leftForward, HIGH);
   digitalWrite(rightEnable, HIGH);
-  digitalWrite(rightForward, HIGH);
-  delay(1000);
-  digitalWrite(leftForward, LOW);
-  digitalWrite(rightForward, LOW);
-  digitalWrite(leftEnable, LOW);
-  digitalWrite(rightEnable, LOW);
-  delay(1000);
-  digitalWrite(leftBackward, HIGH);
-  digitalWrite(leftEnable, HIGH);
-  digitalWrite(leftBackward, HIGH);
-  digitalWrite(rightEnable, HIGH);
-  delay(1000);
-  digitalWrite(leftBackward, LOW);
-  digitalWrite(leftBackward, LOW);
+  analogWrite(leftForward, 0);
+  analogWrite(rightForward, 0);
+  analogWrite(leftBackward, 0);
+  analogWrite(rightBackward, 0);
+}
+void forward()
+{
+  analogWrite(rightForward, power);
+  analogWrite(leftForward, power);
+  analogWrite(rightBackward, 0);
+  analogWrite(leftBackward, 0);
+}
+void backward()
+{
+  analogWrite(leftForward, 0);
+  analogWrite(rightForward, 0);
+  analogWrite(leftBackward, power);
+  analogWrite(rightBackward, power);
 }
